ab1b8ea098a26cb7cca7bda8da4e60db3a2f8843
Marcus Vinicius <marcusv892@gmail.com>
2026-02-20 14:55:44 -0300

    refactor(Sidebar): improve structure and styling of sidebar component

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -9,33 +9,33 @@ import {
 } from "@/components/ui/sidebar";
 import { ChartSpline, Package, ShoppingBasket } from "lucide-react";
 
-function AppSidebar() {
+export default function AppSidebar() {
   return (
-    <Sidebar className="bg-sidebar-primary border-none px-8">
-      <SidebarHeader className="py-6">
-        <h1 className="text-2xl font-black text-neon">ESTOQUEI</h1>
+    <Sidebar className="bg-sidebar-primary border-none">
+      <SidebarHeader className="py-6 px-8">
+        <p className="text-3xl font-black text-neon">ESTOQUEI</p>
       </SidebarHeader>
-      <SidebarContent className="text-gray-sixth">
-        <SidebarGroup className="p-0 text-">
+      <SidebarContent className="text-gray-sixth font-semibold">
+        <SidebarGroup className="p-0 px-2">
           <SidebarMenu className="flex flex-col gap-3">
-            <SidebarMenuItem>
-              <SidebarMenuButton>
+            <SidebarMenuItem className="hover:bg-gray-primary rounded-lg">
+              <SidebarMenuButton className="py-6">
                 <a href="" className="flex w-full gap-2">
                   <ChartSpline />
                   Dashboard
                 </a>
               </SidebarMenuButton>
             </SidebarMenuItem>
-            <SidebarMenuItem>
-              <SidebarMenuButton>
+            <SidebarMenuItem className="hover:bg-gray-primary rounded-lg">
+              <SidebarMenuButton className="py-6">
                 <a href="" className="flex w-full gap-2">
                   <Package />
                   Produtos
                 </a>
               </SidebarMenuButton>
             </SidebarMenuItem>
-            <SidebarMenuItem>
-              <SidebarMenuButton>
+            <SidebarMenuItem className="hover:bg-gray-primary rounded-lg">
+              <SidebarMenuButton className="py-6">
                 <a href="" className="flex w-full gap-2">
                   <ShoppingBasket />
                   Vendas
@@ -48,5 +48,3 @@ function AppSidebar() {
     </Sidebar>
   );
 }
-
-export default AppSidebar;
