0885ddf7202ef21e79c00aea8982839ecc679978
Marcus Vinicius <marcusv892@gmail.com>
2026-02-22 12:41:16 -0300

    feat(Header): enhance header layout and add button for new product

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,3 +1,6 @@
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
+
 interface IHeader {
   title: string;
   subtitle: string;
@@ -5,9 +8,15 @@ interface IHeader {
 
 export default function Header({ title, subtitle }: IHeader) {
   return (
-    <div className="p-8 flex flex-col gap-2">
-      <h1 className="text-gray-sixth">{title}</h1>
-      <p className="text-gray-fifth">{subtitle}</p>
+    <div className="flex w-full justify-between p-8">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-bold text-slate-500">{title}</h1>
+        <p className="text-gray-500">{subtitle}</p>
+      </div>
+      <Button className="bg-neon text-gray-500">
+        <Plus />
+        Novo produto
+      </Button>
     </div>
   );
 }
